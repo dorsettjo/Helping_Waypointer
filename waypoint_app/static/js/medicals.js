030d20b44aa2c2cf608b82f934cd8b0b2c5b405d
TracySanFilipo <tracysanfilipo@gmail.com>
2017-01-10 14:51:07 -0500

    updated legal, medical, and transportation

---
+++ waypoint_app/static/js/medicals.js
@@ -0,0 +1,39 @@
+
+
+function medicalList(){
+        $.getJSON("/api/medicals/").done(function(results){
+            var source = $("#handlebarsTest").html();
+            var template = Handlebars.compile(source);
+            var html = template(results.results);
+            $('#medicalResults').append(html);
+        })
+}
+medicalList()
+
+
+function getDistance(address,id){
+    var splitAddress = address.split(' ')
+    var joinAddress = splitAddress.join('+')
+    var ronald = getCoords(lat, lon)
+    console.log(ronald)
+    // return joinAddress
+    $.getJSON("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=35.7778208,-78.6388908&destinations="+joinAddress).done(function(results){
+        var distance = results['rows'][0]['elements'][0]['distance'].text
+        $("#dist" + id).html(distance)
+    })
+}
+
+function getCoords(lat, lon){
+    console.log('test')
+    console.log(lon)
+    console.log('endtest')
+    return lat + lon;
+}
+
+
+Handlebars.registerHelper("showDistance", function(address, id) {
+    console.log(address)
+    getDistance(address, id)
+    // console.log(distance)
+    // return distance
+})
---
+++ waypoint_app/static/js/transportations.js
@@ -0,0 +1,39 @@
+
+
+function transportationList(){
+        $.getJSON("/api/transportations/").done(function(results){
+            var source = $("#handlebarsTest").html();
+            var template = Handlebars.compile(source);
+            var html = template(results.results);
+            $('#transportationResults').append(html);
+        })
+}
+transportationList()
+
+
+function getDistance(address,id){
+    var splitAddress = address.split(' ')
+    var joinAddress = splitAddress.join('+')
+    var ronald = getCoords(lat, lon)
+    console.log(ronald)
+    // return joinAddress
+    $.getJSON("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=35.7778208,-78.6388908&destinations="+joinAddress).done(function(results){
+        var distance = results['rows'][0]['elements'][0]['distance'].text
+        $("#dist" + id).html(distance)
+    })
+}
+
+function getCoords(lat, lon){
+    console.log('test')
+    console.log(lon)
+    console.log('endtest')
+    return lat + lon;
+}
+
+
+Handlebars.registerHelper("showDistance", function(address, id) {
+    console.log(address)
+    getDistance(address, id)
+    // console.log(distance)
+    // return distance
+})
